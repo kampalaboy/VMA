967eff5b22f984492c28d62b36595de59686e5f2
kampalaboy <ayepak@gmail.com>
2024-02-23 19:10:33 +0300

    UIUX

--- src/App.tsx
+++ src/App.tsx
@@ -106,12 +106,12 @@ const App: React.FC = () => {
        <div id='message-container' className=" bg-teal-100 h-[80vh] w-full relative flex-grow overflow-auto">
       
             {messages.map((message, index) => (
-                <div key={index} className={`${message.role === 'bot' ? 'bg-green-400 text-black rounded-lg mb-[10px] ml-3 p-[10px] lg:max-w-[300px] max-w-[200px]' :
+                <div key={index} className={`${message.role === 'bot' ? 'bg-green-400 text-black rounded-lg mb-[10px] m-3 p-[10px] lg:max-w-[300px] max-w-[200px]' :
                                                                         'bg-teal-600 text-black rounded-lg mb-[10px] p-[10px] lg:max-w-[300px] max-w-[150px] lg:ml-auto lg:mr-1 md:ml-auto md:mr-1 ml-auto mr-3'}`}>
                 {message.content}
                 </div> 
               ))}
-              {loading && <img src="assets/loaders/MrHeartyReads.gif" id="loading" className="w-[100px] h-[100px]"/>}
+              {loading && <img src="assets/loaders/MrHeartyReads.gif" id="loading" className="w-[100px] h-[100px] ml-3"/>}
                <div ref={latestMessage}></div>
         </div>
 
